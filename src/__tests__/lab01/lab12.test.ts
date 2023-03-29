e84e3655c7b44644372c13dc6ac8c1c492fd36f1
Nuno <1222650@isep.ipp.pt>
2023-03-29 19:09:31 +0100

    lab12 ok

--- src/__tests__/lab01/lab12.test.ts
+++ src/__tests__/lab01/lab12.test.ts
@@ -1,4 +1,5 @@
 import { teoremaDeTales } from "../../lab01/lab12";
+
 describe("Teorema de Tales", () => {
   it("Deve calcular corretamente o valor de x", () => {
     const altpredio = 2;
@@ -6,16 +7,16 @@ describe("Teorema de Tales", () => {
     const altupax = 3;
     const sombpax = 6;
     const resultadoEsperado = -1.5;
-    const resultado = teoremaDeTales({ altpredio, sombpredio, altupax, sombpax });
+    const resultado = teoremaDeTales(altpredio, sombpredio, altupax, sombpax);
     expect(resultado).toEqual(resultadoEsperado);
   });
-  it("Deve retornar erro quando denominador é zero", () => {
+  it("Deve retornar null quando denominador é zero", () => {
     const altpredio = 2;
     const sombpredio = 4;
     const altupax = 6;
     const sombpax = 6;
     const resultadoEsperado = sombpredio;
-    const resultado = teoremaDeTales({ altpredio, sombpredio, altupax, sombpax });
+    const resultado = teoremaDeTales(altpredio, sombpredio, altupax, sombpax);
     expect(resultado).toEqual(resultadoEsperado);
   });
 });
